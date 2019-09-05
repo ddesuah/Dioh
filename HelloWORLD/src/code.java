9bbe2eefe2ee205afa51a6b59271421e579cc29c
diohdesuah <diohdesuah@10.242.0.166>
2019-09-05 12:16:06 -0400

    okay

---
+++ HelloWORLD/src/code.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Project Name: Hello World
+public class code {
+	
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+System.out.println("I want to live to be a champ");
+int a=1;
+for(int i =0;i<10;i++) {
+	System.out.print(a+")");
+	System.out.println("Dioh is a novice coder");
+	a++;
+			}
+	}
+}
