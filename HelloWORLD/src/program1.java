a54af84642c4ae3f041f5e325d29f3f153777df5
diohdesuah <diohdesuah@10.242.0.166>
2019-09-10 11:45:20 -0400

    This is program 1 homework

---
+++ HelloWORLD/src/program1.java
@@ -0,0 +1,25 @@
+
+public class program1 {
+/* Project Name :
+ * Author:
+ * Last Modified:
+ *  Version:
+ *  
+ *  
+ *  
+ *  
+ *  
+ *  
+*/
+	public static void main(String[] args) {
+		String name = "dioh";
+		int age = 18;
+		String favMovie = "Avengers: Endgame";
+		
+		
+		System.out.println("Hello Brad, the programmer of this code is " + name + ".");
+		System.out.println("His favorite movie is " + favMovie + ".");
+		System.out.print("He is " + age + " years old.");
+	}
+
+}
