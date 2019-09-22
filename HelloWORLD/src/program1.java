a2ff2b9827d278492428c5df7bc97bdd273586c6
diohdesuah <diohdesuah@10.242.0.166>
2019-09-22 10:51:50 -0400

    this is my homework

--- HelloWORLD/src/program1.java
+++ HelloWORLD/src/program1.java
@@ -1,25 +1,26 @@
 
 public class program1 {
-/* Project Name :
- * Author:
- * Last Modified:
- *  Version:
- *  
- *  
- *  
- *  
- *  
- *  
+/* Project Name : Program 1 
+ * Author: Dioh Desuah
+ * Last Modified: Sept 10, 2019
+ *  Version: 1.1 
 */
 	public static void main(String[] args) {
-		String name = "dioh";
-		int age = 18;
-		String favMovie = "Avengers: Endgame";
+		String name = "Dioh"; // stores my name
+		byte age = 18; //stores my age
+		String favMovie = "Avengers: Endgame"; //stores my favorite movie
+		boolean tired = true;
 		
-		
-		System.out.println("Hello Brad, the programmer of this code is " + name + ".");
+		//
+		System.out.println("Hello Brad, the programmer of this code is " + name + "."); // prints out the sentence
 		System.out.println("His favorite movie is " + favMovie + ".");
-		System.out.print("He is " + age + " years old.");
+		System.out.print("He is " + age + " years old");
+		if(tired) {// I wanted to show off where I am in Java, so i used if statement
+			System.out.print(" and he is tired.");
+		}
+		else {
+			System.out.print(" and he is energized.");
+		}
 	}
 
 }
